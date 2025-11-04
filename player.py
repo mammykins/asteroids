0f25f76f98d3a97622ff914156dc1eae595c449d
Matthew Gregory <mammykins1337@gmail.com>
2025-11-04 22:46:11 +0800

    Add Player movement

--- player.py
+++ player.py
@@ -24,6 +24,10 @@ class Player(circleshape.CircleShape):
         # sub-classes must override
         pygame.draw.polygon(screen, "white", self.triangle(), width=2)
 
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
@@ -34,3 +38,7 @@ class Player(circleshape.CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
